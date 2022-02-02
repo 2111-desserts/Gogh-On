33df39b423a312f2caf54a197f94b8b7d19d9fac
Amanda Henneberry <amandahenneberry@icloud.com>
2022-02-02 16:59:46 -0500

    'adding socket to server - still working on chat & socket"

--- server/index.js
+++ server/index.js
@@ -1,13 +1,39 @@
 const PORT = process.env.PORT || 8080;
 const app = require('./app');
 
-const init = async () => {
-  try {
-    // start listening (and create a 'server' object representing our server)
-    app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`));
-  } catch (ex) {
-    console.log(ex);
-  }
-};
+// //From og fullstack boilerplate (NOT working):
+// const socket = require('socket.io');
 
-init();
+// const init = async () => {
+//   try {
+//     // start listening (and create a 'server' object representing our server)
+//     app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`));
+//   } catch (ex) {
+//     console.log(ex);
+//   }
+// };
+
+const server = app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`);
+});
+
+
+const serverSocket = require('socket.io')(server)
+serverSocket.on('connection', (socket) => {
+	console.log(`Connection from client ${socket.id}`);
+	// socket.on('send-message', (message, room) => {
+	// 	if (room === '') {
+	// 		socket.broadcast.emit('receive-message', message);
+	// 	} else {
+	// 		socket.to(room).emit('receive-message', message);
+	// 	}
+	// });
+	socket.on('join-room', (room, cb) => {
+		socket.join(room);
+		// socket.broadcast.emit('joined-room', user);
+		cb(`joined ${room}`);
+	});
+});
+
+//from OG fullstack boilerplate
+// init();
