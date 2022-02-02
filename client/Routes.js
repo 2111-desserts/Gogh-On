23ba9dd45817f2fe65ac467d39184518a7485494
aamponsah97 <ataaamponsah97@gmail.com>
2022-02-01 22:47:00 -0500

    PostDraw component renders with hello world

--- client/Routes.js
+++ client/Routes.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux'
 import {withRouter, Route, Switch, Redirect} from 'react-router-dom'
 import { Login, Signup } from './components/AuthForm';
 import Home from './components/Home';
+import PostDraw from './components/PostDraw';
 import {me} from './store'
 
 /**
@@ -26,6 +27,7 @@ class Routes extends Component {
         ) : (
           <Switch>
             <Route path='/' exact component={ Login } />
+            <Route path="/postdraw" component={PostDraw} />
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
           </Switch>
---
+++ client/components/PostDraw.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class PostDraw extends Component {
+  constructor(){
+    super()
+  }
+
+  render(){
+    return(
+      <div id="post-draw-container">
+        <h3>Hello world!</h3>
+        <button>okay i like it, picasso</button>
+      </div>
+    )
+  }
+}
+
+export default PostDraw
