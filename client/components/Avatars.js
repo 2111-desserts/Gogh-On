62021adff252236560296644815a93943cf1c438
Ellie <elliel.4797@gmail.com>
2022-02-07 11:21:10 -0500

    Delete Avatars.js

--- client/components/Avatars.js
+++
@@ -1,6 +0,0 @@
-export const avatars = [
-  'https://garticphone.com/images/avatar/41.svg',
-  'https://garticphone.com/images/avatar/6.svg',
-  'https://garticphone.com/images/avatar/39.svg',
-  'https://garticphone.com/images/avatar/2.svg',
-];
