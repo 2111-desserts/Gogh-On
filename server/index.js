1323a6a65ba1c42bfc686005bc13e45a7263a852
Quynh Le <nhuqxl@gmail.com>
2022-02-08 10:22:36 -0800

    testing mousedown

--- server/index.js
+++ server/index.js
@@ -15,8 +15,13 @@ serverSocket.on('connection', (socket) => {
   socket.on('backend-test', (message) => {
     console.log(message);
   });
-  socket.on('is-drawing', (data) => {
-    socket.broadcast.emit('is-drawing', data);
+  socket.on('is-drawing', (lines) => {
+    socket.broadcast.emit('is-drawing', lines);
+  });
+
+  socket.on('on-down', (data) => {
+    socket.broadcast.emit('on-down', data);
+    console.log(data);
   });
 
   socket.on('send-message', (message, sendingUser) => {
